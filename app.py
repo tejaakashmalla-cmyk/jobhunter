ea54ece4c2a5e18c3f7605694c5cda7d6a1dd536
malla teja akash <you@example.com>
2026-04-03 20:17:31 +0530

    fixed root route

--- app.py
+++ app.py
@@ -200,4 +200,4 @@ def send_otp(email, otp):
 
 # ================= RUN =================
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000) 
